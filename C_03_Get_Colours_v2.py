f0f97d7303aa804ebd498d4cb2341f442ed9a905
J-Gottschalk-NZ <49209767+J-Gottschalk-NZ@users.noreply.github.com>
2025-01-21 13:36:40 +1300

    Working game component.  Need stats & hints

--- C_03_Get_Colours_v2.py
+++ C_03_Get_Colours_v2.py
@@ -3,6 +3,11 @@ import random
 
 
 def round_ans(val):
+    """
+    Rounds numbers to nearest integer
+    :param val: number to be rounded.
+    :return: Rounded number (an integer)
+    """
     var_rounded = (val * 2 + 1) // 2
     raw_rounded = "{:.0f}".format(var_rounded)
     return int(raw_rounded)
